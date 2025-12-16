c89ecc7779d36d61b6f5c05a9a2d6f6f770740ce
JATIN123455 <143105459+JATIN123455@users.noreply.github.com>
2025-12-16 21:52:27 +0530

    Create clean.py

---
+++ data/src/clean.py
@@ -0,0 +1,7 @@
+Data Cleaning Logic
+import pandas as pd
+
+def clean_data(df):
+    df.columns = [c.lower().strip() for c in df.columns]
+    df = df.drop_duplicates(subset=["company", "round"])
+    return df
